3966db104b2e981168cc6dc843f398b412e229e8
bartoszprogramista <bartekkosmider98@gmail.com>
2017-09-12 23:27:41 +0200

    ;p;p;

--- 12.09/script.js
+++ 12.09/script.js
@@ -22,9 +22,23 @@ console.log(poleProstokata);*/
 
 function funkcja()
 {
+    if(document.getElementById("1").checked)
+        {
     var promien = document.form.promien.value;
     var pole = promien * promien * Math.PI;
     var objetosc = 2 * promien * Math.PI;
     var wynik = document.getElementById("wynik");
     wynik.textContent = "pole = "+ pole+ " objetosc = "+ objetosc;
+        }
+    else if(document.getElementById("2").checked)
+    {
+        console.log("no elo");
+        var promien =document.form.promiens.value;
+        var wysokosc = document.form.wysokoscs.value;
+        var costam = wysokosc * promien;
+        document.getElementById("wynik");
+        wynik.textContent = "stozek : "+costam;
+    }
+    else
+        document.write("gówno");
 }
