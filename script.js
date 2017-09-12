fd8288b041caeeb5f114e6c60c7ed3f9ba1e2f57
bartoszprogramista <bartekkosmider98@gmail.com>
2017-09-12 12:13:29 +0200

    sroda

---
+++ script.js
@@ -0,0 +1,22 @@
+function poleObjetosc(szerokosc, dlugosc, wysokosc)
+{
+    var pole = szerokosc * dlugosc;
+    var objetosc = pole * wysokosc;
+    var wynik = [pole, objetosc];
+    return wynik;
+}
+
+var a = prompt("podaj szerokosc");
+var b = prompt("podaj długość");
+var c = prompt("podaj wysokość");
+
+var x=prompt("podaj wartosc","0 pole, 1 objetosc");
+
+
+var poleProstokata = poleObjetosc(a,b,c)[x]; //[] zwraca okreslony element tablicy
+console.log(poleProstokata);
+
+//napisać program, który oblicza obwód, pole koła oraz objętość stożka. dane uzytkownik podaje z klawy, wykorzystaj obiekt math. uzytkownik wybiera za pomocą pola radio button co chce obliczyc
+
+
+
