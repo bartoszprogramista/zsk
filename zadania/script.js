33304e92756ee1f6f34f3f87424959f30736e7af
bartoszprogramista <bartekkosmider98@gmail.com>
2017-09-19 10:41:55 +0200

    iyyggyu

--- zadania/script.js
+++ zadania/script.js
@@ -1,15 +1,115 @@
-function kolo(promien)
-{
-    var obwod = 2*3*promien;
-
-    return obwod;
-
-};
+var pi = Math.PI;
+console.log(pi);
 
 
+/*var x = prompt("odaj wartosc");
+console.log(Math.sqrt(x));*/
+
+var a = 13.2;
+var b = -28.5;
+var c = 1;
+
+var minimalna = Math.min(a,b,c);
+console.log(minimalna);
+
+var maksymalna = Math.max(a,b,c);
+console.log(maksymalna);
+
+var zaokroglenie = Math.round(b);
+
+console.log(zaokroglenie);
+
+//abs, round, floor
+
+var x = -56;
+var y = -13.1;
+var z = 14.9;
+
+document.write("<br/>"+ Math.abs(x) + "<br/>");
+document.write("<br/>"+ Math.abs(y)+ "<br/>");
+document.write("<br/>"+ Math.abs(z)+ "<br/>");
+
+document.write("<br/>"+ Math.round(x) + "<br/>");
+document.write("<br/>"+ Math.round(y)+ "<br/>");
+document.write("<br/>"+ Math.round(z)+ "<br/>");
+
+document.write("<br/>"+ Math.floor(x) + "<br/>");
+document.write("<br/>"+ Math.floor(y)+ "<br/>");
+document.write("<br/>"+ Math.floor(z)+ "<br/>");
 
 
-var promien = prompt("podaj promien");
-console.log(promien);
-console.log(kolo);
-document.write(obwod);
+
+
+//potęgowanie
+
+
+var p = Math.pow(2,10);
+console.log(p);
+
+
+//losowanie
+
+var losuj = Math.random();
+console.log(losuj);
+
+var k = 3.343553523454;
+k = k.toFixed(2);
+document.write(k + "<br/>");
+console.log(typeof(k));
+
+
+
+var j = 3.343553523454;
+console.log(typeof(j));
+j = j.toPrecision(2);
+document.write(j);
+console.log(typeof(j));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
