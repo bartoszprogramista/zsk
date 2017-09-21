d16c94c49a86a8604561a11f95f163a256e4d369
bartoszprogramista <bartekkosmider98@gmail.com>
2017-09-21 19:45:24 +0200

    6969

--- 20.09/egzamin.js
+++ 20.09/egzamin.js
@@ -10,12 +10,93 @@ var elDzielenie = document.getElementById('dzielenie');
 
 var elWynik = document.getElementById('wynik');
 
+
 elDodawanie.onclick = function(){
-    if(elPierwszy.value == '' || elDrugi.value =='' ){
+    elPierwszy = parseFloat(elPierwszy.value);
+    elDrugi = parseFloat(elDrugi.value);
+    console.log(elPierwszy);
+    console.log(elDrugi);
+
+    if (isNaN(elPierwszy) || isNaN(elDrugi)){
+
         elWynik.innerHTML = 'prosze wprowadzic cokolwiek';
     }
+
+/*    else if(elPierwszy.value == 0 || elDrugi.value ==0 ){
+            alert('nie dzielimy przez zero');
+    }*/
+
     else{
         var wynik = parseFloat(elPierwszy+elDrugi);
         elWynik.innerHTML = wynik;
     }
 }
+
+elOdejmowanie.onclick = function(){
+    elPierwszy = parseFloat(elPierwszy.value);
+    elDrugi = parseFloat(elDrugi.value);
+    console.log(elPierwszy);
+    console.log(elDrugi);
+
+    if (isNaN(elPierwszy) || isNaN(elDrugi)){
+
+        elWynik.innerHTML = 'prosze wprowadzic cokolwiek';
+    }
+
+/*    else if(elPierwszy.value == 0 || elDrugi.value ==0 ){
+            alert('nie dzielimy przez zero');
+    }*/
+
+    else{
+        var wynik = parseFloat(elPierwszy - elDrugi);
+        elWynik.innerHTML = wynik;
+    }
+}
+
+elMnozenie.onclick = function(){
+    elPierwszy = parseFloat(elPierwszy.value);
+    elDrugi = parseFloat(elDrugi.value);
+    console.log(elPierwszy);
+    console.log(elDrugi);
+
+    if (isNaN(elPierwszy) || isNaN(elDrugi)){
+
+        elWynik.innerHTML = 'prosze wprowadzic cokolwiek';
+    }
+
+/*    else if(elPierwszy.value == 0 || elDrugi.value ==0 ){
+            alert('nie dzielimy przez zero');
+    }*/
+
+    else{
+        var wynik = parseFloat(elPierwszy * elDrugi);
+        elWynik.innerHTML = wynik;
+    }
+}
+
+elDzielenie.onclick = function(){
+    elPierwszy = parseFloat(elPierwszy.value);
+    elDrugi = parseFloat(elDrugi.value);
+    console.log(elPierwszy);
+    console.log(elDrugi);
+
+    if (isNaN(elPierwszy) || isNaN(elDrugi)){
+
+        elWynik.innerHTML = 'prosze wprowadzic cokolwiek';
+    }
+
+    else if(elPierwszy == 0 || elDrugi ==0 ){
+            elWynik.innerHTML = 'nie dzielimy przez zero';
+
+    }
+
+    else{
+        var wynik = parseFloat(elPierwszy / elDrugi);
+        elWynik.innerHTML = wynik;
+    }
+}
+
+
+
+
+
